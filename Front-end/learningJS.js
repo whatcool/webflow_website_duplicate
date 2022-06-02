78342dc5cd6b6389ad1da36ddc805b7eb858c68f
shivam <shivamgupta16112001@gmail.com>
2022-06-02 18:03:24 +0530

    Angela front-end started

---
+++ Front-end/learningJS.js
@@ -0,0 +1,48 @@
+// console.log("sdfjo");
+// var num1 = 2
+// var num2 = 3
+// console.log(num1 + num2);
+// var marks = {
+//     name: "Shivam",
+//     RollNo: 64
+// };
+
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getTime());
+// console.log(myDate.getFullYear());
+
+// var count = 10;
+
+// function timer() {
+//     if (count == 0) {
+//         clearInterval(id);
+//         return;
+//     }
+//     count--;
+//     console.log(count);
+// }
+
+// var id = setInterval(timer, 1000);
+
+// function sayHello() {
+//     alert("heloo");
+// }
+
+// var btn = document.querySelector("#btn")
+// btn.addEventListener('click', sayHello);
+
+console.log('hey');
+// var outerDiv = document.getElementById('outer');
+
+// outerDiv.addEventListener('mouseover', function() {
+//     console.log('mosuer over');
+// })
+
+// outerDiv.addEventListener('mouseout', function() {
+//     console.log('mosuer out');
+// })
+
+// outerDiv.addEventListener('click', function() {
+//     console.log('div is clicked')
+// })
