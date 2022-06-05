c32239a86b4dda3fc224164dec415dfa2e7b9a79
shivam <shivamgupta16112001@gmail.com>
2022-06-05 23:18:30 +0530

    BMI and Calulator finalized

--- Back-end/my-express-server/calulator-server.js
+++ Back-end/my-express-server/calulator-server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/calculator-index.html");
 })
 
 app.post("/", function(req, res) {
